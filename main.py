4646e97742a3c5872e82ad3d67df9ba7f770770a
Vitaly Andrejeus <andrejeusv@gmail.com>
2026-07-03 09:36:06 -0700

    Expanded with first task and initial method

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 class CodingAgent:
-    def run(self):
-        print("Agent is running")
+    def __init__(self, task): #store task parameter as attribute upon initiation
+        self.task = task
+    def run(self):  
+        print(self.task)
 
-agent = CodingAgent()
+agent = CodingAgent("Create a Python calculator")
 
 agent.run()
